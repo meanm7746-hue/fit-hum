d09257f1e364e079e9adaf983a47b323164402da
meanm7746-hue <meanm7746@gmail.com>
2026-05-10 02:35:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ addDoc,
 onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-/* 🔥 FIREBASE CONFIG */
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
+apiKey: "YOUR_KEY",
 authDomain: "YOUR_DOMAIN",
-projectId: "YOUR_PROJECT_ID"
+projectId: "YOUR_ID"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,28 +18,24 @@ const db = getFirestore(app);
 /* ADD PRODUCT */
 window.addProduct = async function(){
 await addDoc(collection(db,"products"),{
-name: name.value,
-price: price.value,
-img: img.value,
-time: Date.now()
+name:name.value,
+price:price.value,
+img:img.value
 });
-alert("Product Added 🚀");
 }
 
-/* LIVE PRODUCTS */
+/* LOAD PRODUCTS */
 onSnapshot(collection(db,"products"),(snap)=>{
-let grid = document.getElementById("grid");
+let grid=document.getElementById("grid");
 grid.innerHTML="";
 
 snap.forEach(d=>{
-let p = d.data();
-
-grid.innerHTML += `
-<div class="card">
-<img src="${p.img}">
+let p=d.data();
+grid.innerHTML+=`
+<div>
+<img src="${p.img}" width="100%">
 <h3>${p.name}</h3>
 <p>₹${p.price}</p>
-</div>
-`;
+</div>`;
 });
 });
