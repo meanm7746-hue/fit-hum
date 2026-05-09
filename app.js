c59aad1d47dd16c1cf5d7b64b9c40e3fcf73fc56
meanm7746-hue <meanm7746@gmail.com>
2026-05-10 02:41:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-getFirestore,
-collection,
-addDoc,
-onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+let cart = [];
 
-const firebaseConfig = {
-apiKey: "YOUR_KEY",
-authDomain: "YOUR_DOMAIN",
-projectId: "YOUR_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-/* ADD PRODUCT */
-window.addProduct = async function(){
-await addDoc(collection(db,"products"),{
-name:name.value,
-price:price.value,
-img:img.value
-});
+window.addToCart = function(name, price){
+cart.push({name, price});
+renderCart();
 }
-
-/* LOAD PRODUCTS */
-onSnapshot(collection(db,"products"),(snap)=>{
-let grid=document.getElementById("grid");
-grid.innerHTML="";
-
-snap.forEach(d=>{
-let p=d.data();
-grid.innerHTML+=`
-<div>
-<img src="${p.img}" width="100%">
-<h3>${p.name}</h3>
-<p>₹${p.price}</p>
-</div>`;
-});
-});
